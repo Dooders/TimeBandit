a7fff16afe4a13bafdb418dd01dddb32fa1f3734
Chris Mangum <csmangum@gmail.com>
2024-06-02 21:03:24 -0700

    chore: Update Graph class to manage the space of objects

---
+++ bandit/graph.py
@@ -0,0 +1,101 @@
+import networkx as nx
+
+from bandit.state import Object
+
+
+class Graph(nx.DiGraph):
+    """
+    Space class is a directed graph that represents the space of objects.
+    It is a subclass of networkx.DiGraph.
+    """
+
+    def __init__(self):
+        super().__init__()
+
+    def add_object(self, object: "Object") -> None:
+        """
+        Adds an object to the space
+
+        Parameters
+        ----------
+        object (Object):
+            The object to add to the space
+        """
+        self.add_node(object)
+
+    def remove_object(self, object: "Object") -> None:
+        """
+        Removes an object from the space
+
+        Parameters
+        ----------
+        object (Object):
+            The object to remove from the space
+        """
+        self.remove_node(object)
+
+    def get_state(self, object_id) -> dict:
+        """
+        Returns the state of the object
+
+        Parameters
+        ----------
+        object_id (str):
+            The id of the object
+
+        Returns
+        -------
+        dict:
+            The state of the object
+        """
+        object_node = self.nodes(object_id)
+        return dict(object_node)
+
+    def get_object(self, object_id) -> dict:
+        """
+        Returns the object
+
+        Parameters
+        ----------
+        object_id (str):
+            The id of the object
+
+        Returns
+        -------
+        dict:
+            The object
+        """
+        return self.nodes(object_id)
+
+    def draw_space(self) -> None:
+        nx.draw(self, with_labels=True, font_weight="bold")
+
+    def update(self, input) -> dict:
+        """
+        Updates the object state
+
+        #! Need to work out inputs and outputs
+        #! Need a better counter that different classes can use
+
+        - Iter through every node in the space
+        - Update the object
+        - Return the state of the object
+        - Insert into temporal graph
+        - Connect the threads between current state and previous state
+        - Finalize update
+        - Return the state of the object
+        """
+        for object in self.nodes:
+            object.update()
+
+        return self.state
+
+    @property
+    def state(self) -> dict:
+        """
+        Returns
+        -------
+        dict:
+            The state of the object
+        """
+        return self.nodes
