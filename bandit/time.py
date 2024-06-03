868b35fae7f1005930d4fc9712cfd4544fd6ea4f
Chris Mangum <csmangum@gmail.com>
2024-06-02 21:03:16 -0700

    chore: Refactor Time class to use custom Graph class for managing the time of objects

--- bandit/time.py
+++ bandit/time.py
@@ -1,93 +1,46 @@
-import networkx as nx
+from bandit.graph import Graph
+from bandit.state import Object
 
 
-class Time(nx.DiGraph):
+class Time(Graph):
     """
     Time class is a directed graph that represents the time of objects.
     It is a subclass of networkx.DiGraph.
 
     Methods
     -------
-    add_object(object):
-        Adds an object to the time
-    remove_object(object):
-        Removes an object from the time
     add_thread(object1, object2):
         Adds a thread between two objects
     remove_thread(object1, object2):
         Removes a thread between two objects
-    get_state(object_id):
-        Returns the state of the object
-    get_object(object_id):
-        Returns the object
-    draw_time():
-        Draws the time
-    update():
-        Updates the object state
     """
 
     def __init__(self):
         super().__init__()
-        self._input = None
-        self._output = None
-        self._step = 0
-        self._cycle = 0
         self.time = int(f"{self._cycle}.{self._step}")
 
-    def add_object(self, object) -> None:
-        """
-        Adds an object to the time
-
-        Parameters
-        ----------
-        object (Object):
-            The object to add to the time
-        """
-        self.add_node(object)
-
-    def remove_object(self, object) -> None:
-        """
-        Removes an object from the time
-
-        Parameters
-        ----------
-        object (Object):
-            The object to remove from the time
-        """
-        self.remove_node(object)
-
-    def add_thread(self, object1, object2) -> None:
+    def add_thread(self, object1: "Object", object2: "Object") -> None:
         """
         Adds a thread between two objects
+
+        Parameters
+        ----------
+        object1 (Object):
+            The first object
+        object2 (Object):
+            The second object
         """
         self.add_edge(object1, object2)
 
-    def remove_thread(self, object1, object2) -> None:
+    def remove_thread(self, object1: "Object", object2: "Object") -> None:
         """
         Removes a thread between two objects
+
+        Parameters
+        ----------
+        object1 (Object):
+            The first object
+        object2 (Object):
+            The second object
         """
         self.remove_edge(object1, object2)
-
-    def get_state(self, object_id) -> None:
-        """
-        Returns the state of the object
-        """
-        return self.nodes[object_id]
-
-    def get_object(self, object_id) -> None:
-        """
-        Returns the object
-        """
-        return self.nodes[object_id]
-
-    def draw_time(self) -> None:
-        """
-        Draws the time
-        """
-        pass
-
-    def update(self) -> None:
-        """
-        Updates the object state
-        """
-        pass
