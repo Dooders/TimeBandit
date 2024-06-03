5ea33c89ff5270195d85c5f62912ee5259758e08
Chris Mangum <csmangum@gmail.com>
2024-06-02 21:30:55 -0700

    Refactor TimeBandit class to include tick method for time tracking

--- bandit/main.py
+++ bandit/main.py
@@ -1,1 +1,16 @@
+from bandit.space import Space
+from bandit.time import Time
 
+
+
+
+class TimeBandit:
+    def __init__(self):
+        self.time_graph = Time()
+        self.space_graph = Space()
+
+    def tick(self):
+        self._time += 1
+
+    def get_time(self):
+        return self._time
