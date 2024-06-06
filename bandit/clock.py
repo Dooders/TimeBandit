2b70aec74665d53aa921476e5465f794bb56e9e8
Chris Mangum <peril10@hotmail.com>
2024-06-05 17:03:29 -0700

    "Add clone method to Clock class and include starting_time parameter in constructor"

--- bandit/clock.py
+++ bandit/clock.py
@@ -1,7 +1,5 @@
 import time
 
-from bandit.object import Object
-
 
 class Clock:
     """
@@ -9,7 +7,7 @@ class Clock:
 
     Methods
     --------
-    step(object)
+    step()
         Steps the clock by 1 step.
         Updates the object's step and cycle properties.
 
@@ -25,38 +23,45 @@ class Clock:
         The real time since the clock was started.
     """
 
-    def __init__(self, steps_per_cycle: int) -> None:
+    def __init__(self, steps_per_cycle: int, starting_time: str = "1.0") -> None:
+        """
+        Initializes the clock.
+
+        Parameters
+        ----------
+        steps_per_cycle
+            The number of steps per cycle.
+        starting_time
+            The starting time in the format of "{cycle}:{step}".
+        """
         self.steps_per_cycle = steps_per_cycle
+        self.starting_time = starting_time
         self._cycle = 1
         self._step = 0
         self._start_time = time.time()
 
-    def step(self, object: "Object") -> None:
+    def step(self) -> None:
         """
         Steps the clock by 1 step.
         Updates the object's step and cycle properties.
-
-        Parameters
-        ----------
-        object
-            The object to update.
         """
         self._step += 1
         if self._step >= self.steps_per_cycle:
             self._cycle += 1
             self._step = 0
-
-        # update the object's step and cycle properties
-        object.step = self._step
-        object.cycle = self._cycle
-
-    @property
-    def cycle(self) -> int:
-        return self._cycle
-
-    @property
-    def step(self) -> int:
-        return self._step
+            
+        return self._cycle, self._step
+    
+    def clone(self) -> "Clock":
+        """
+        Returns a clone of the clock with the same steps_per_cycle and current time in the simulation
+        
+        Returns
+        --------
+        Clock
+            A clone of the clock with the same steps_per_cycle and current time in the simulation
+        """
+        return Clock(self.steps_per_cycle, self.starting_time)
 
     @property
     def time(self) -> float:
