5b07db9cd23e84fec36ed581a69d81d79cf53495
Chris Mangum <peril10@hotmail.com>
2024-06-05 17:04:12 -0700

    Refactor Time class to use root_id_cache instead of temporal_id_cache

--- bandit/time.py
+++ bandit/time.py
@@ -1,5 +1,8 @@
 from bandit.graph import Graph
+from typing import TYPE_CHECKING
 
+if TYPE_CHECKING:
+    from bandit.object import Object
 
 class Time(Graph):
     """
@@ -10,7 +13,7 @@ class Time(Graph):
     ----------
     time (str):
         The time of the time
-    temporal_id_cache (dict):
+    root_id_cache (dict):
         A dictionary that maps temporal ids to objects
 
     Methods
@@ -25,8 +28,7 @@ class Time(Graph):
 
     def __init__(self):
         super().__init__()
-        self.time = f"Time: {self.cycle} {self.step}"
-        self.temporal_id_cache = {}
+        self.root_id_cache = {}
 
     def add_thread(self, object_a: "Object", object_b: "Object") -> None:
         """
@@ -64,22 +66,20 @@ class Time(Graph):
             The state of the space
         """
         new_temporal_cache = {}
-
+        
         for object in space_state.values():
             temporal_id = object.get("temporal_id", 0)
             root_id = object.get("root_id", None)
             self.add_node(temporal_id, **object)
             new_temporal_cache[temporal_id] = root_id
-            # print(f"temporal_id: {temporal_id}, root_id: {root_id}")
 
-        # Add threads to any matching root_ids in the temporal_id_cache
-        for temporal_id, root_id in self.temporal_id_cache.items():
+        # Add threads to any matching root_ids in the root_id_cache
+        for temporal_id, root_id in self.root_id_cache.items():
             if root_id in space_state:
                 self.add_thread(temporal_id, space_state[root_id])
 
-        self.temporal_id_cache = new_temporal_cache
-        
-        print(f"new_temporal_cache: {new_temporal_cache}")
+        self.root_id_cache = new_temporal_cache
+
 
     def threads(self) -> list:
         """
