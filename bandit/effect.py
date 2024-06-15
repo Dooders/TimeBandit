d759ba2301f5f5c84ced9ec4f3aad46871766c2a
Chris Mangum <csmangum@gmail.com>
2024-06-15 15:42:53 -0700

    Update docstring in effect.py to clarify the concept of effects in simulations

---
+++ bandit/effect.py
@@ -0,0 +1,14 @@
+
+"""
+A persistent, conditional, or temporal effect that influences an object during 
+it's update step.
+
+An effect is the result of an interaction in the simulation, like a 
+force being applied to a ball in space, or a change in temperature in a room.
+
+The interaction of force on the object incurs an effect on the object that 
+continues based on the rules of the effect.
+
+A ball in space would continue forever in the direction opposite the force
+applied to it, given no other influences or interactions.
+"""
