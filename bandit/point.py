7eaf49074178877a3f7837b63691af5b66261695
Chris Mangum <csmangum@gmail.com>
2024-06-15 15:43:38 -0700

    Refactor comments in point.py for clarity

---
+++ bandit/point.py
@@ -0,0 +1,6 @@
+"""
+Point is a specialized type of node that represents a specific step in time of the
+simulation.
+
+Spaces and Objects are anchored to a point in time. An Anchor is a specialized edge
+"""
