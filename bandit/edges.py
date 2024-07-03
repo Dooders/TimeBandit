f1729882cc223e0b82e17260d10c3f54a0ade1f3
Chris Mangum <csmangum@gmail.com>
2024-07-03 16:21:51 -0700

    Delete bandit/edges.py

--- bandit/edges.py
+++
@@ -1,9 +0,0 @@
-"""
-Specialized Edge classes for Graphs
-
-Space:
-- Connection: Represents the relationships between objects in a space at a point in time.
-    The relationship system
-- Interaction: Represents the interactions between objects in a space at a point in time.
-    The event system
-"""
