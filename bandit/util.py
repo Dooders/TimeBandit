5a89cff12f71b1f6c44365e5b344507732f3f1c7
Chris Mangum <csmangum@gmail.com>
2024-07-08 18:43:06 -0700

    feat: Add LimitedDict class with item limit functionality
    - Added `LimitedDict` class extending `OrderedDict` to enforce a limit on the number of items stored.
    - Implemented `__setitem__` to remove the oldest item when the limit is reached.
    - Implemented `__contains__` to check for key existence.
    - Imported necessary modules and types.

--- bandit/util.py
+++ bandit/util.py
@@ -1,4 +1,9 @@
 import hashlib
+from collections import OrderedDict
+from typing import TYPE_CHECKING, Any
+
+if TYPE_CHECKING:
+    from bandit.state import State
 
 
 def generate_hash(object_state) -> str:
@@ -17,10 +22,43 @@ def generate_hash(object_state) -> str:
     str:
         The hash of the object
     """
-    
+
     object_state_str = str(object_state)
     state_bytes = object_state_str.encode("utf-8")
 
     hash_md5 = hashlib.md5()
     hash_md5.update(state_bytes)
     return hash_md5.hexdigest()
+
+
+class LimitedDict(OrderedDict):
+    """
+    A dictionary with a limit on the number of items it can store.
+
+    When the limit is reached, the oldest item is removed.
+
+    Parameters
+    ----------
+    limit : int
+        The maximum number of items to store.
+    """
+
+    def __init__(self, limit: int, *args, **kwargs) -> None:
+        self.limit = limit
+        super().__init__(*args, **kwargs)
+
+    def __setitem__(self, key: str, value: Any) -> None:
+        """
+        Sets the value of the key.
+        """
+        if key in self:
+            del self[key]
+        elif len(self) >= self.limit:
+            self.popitem(last=False)
+        super().__setitem__(key, value)
+
+    def __contains__(self, key: str) -> bool:
+        """
+        Returns whether the key is in the dictionary.
+        """
+        return key in self.keys()
